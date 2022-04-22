216074f0aefc15f3c4a92adf4621d4844d7f44fb
pecodeliar <92639084+pecodeliar@users.noreply.github.com>
2022-04-22 07:18:55 -0400

    Adjusted Graphic Position and Fixed Card Button

--- app/src/main/java/com/example/googlemock/screen_collections/components/PlaceholderCard.kt
+++ app/src/main/java/com/example/googlemock/screen_collections/components/PlaceholderCard.kt
@@ -9,6 +9,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -17,6 +18,7 @@ import androidx.compose.ui.unit.sp
 import com.example.googlemock.R
 import com.example.googlemock.screen_collections.model.Placeholder
 import com.example.googlemock.ui.theme.Accent
+import com.example.googlemock.ui.theme.CardButton
 import com.example.googlemock.ui.theme.ShowsCard
 
 //The card that shows when there hasn't been anything saved
@@ -25,12 +27,14 @@ fun PlaceholderCard(placeholder: Placeholder) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .height(208.dp)
-            .clip(RoundedCornerShape(15.dp)),
+            .height(213.dp)
+            .clip(RoundedCornerShape(15.dp))
+            .padding(10.dp),
         backgroundColor = placeholder.background
     ) {
         Row(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier
+                .fillMaxSize(),
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
             Column(modifier = Modifier
@@ -52,16 +56,22 @@ fun PlaceholderCard(placeholder: Placeholder) {
                     fontWeight = FontWeight.Bold,
                     modifier = Modifier
                         .padding(bottom = 8.dp),
-                    color = Color.White
+                    color = Color.White,
+                    style = LocalTextStyle.current.copy(lineHeight = 25.sp)
                 )
                 Button(
                     onClick = { /*TODO*/ },
                     modifier = Modifier
-                        .height(20.dp)
-                        .clip(RoundedCornerShape(10.dp)),
-                    colors = ButtonDefaults.buttonColors(backgroundColor = Color.DarkGray)
+                        .padding(top = 10.dp, bottom = 10.dp),
+                    shape = RoundedCornerShape(50),
+                    colors = ButtonDefaults.buttonColors(backgroundColor = CardButton)
                 ) {
-                    Text(text = "Go to ${placeholder.button}")
+                    Text(
+                        text = "Go to ${placeholder.button}",
+                        color = Color.White,
+                        fontWeight = FontWeight.Light,
+                        letterSpacing = 0.5.sp
+                    )
                 }
             }
             Column(
@@ -70,7 +80,9 @@ fun PlaceholderCard(placeholder: Placeholder) {
             ) {
                 Image(
                     painterResource(placeholder.graphic),
+                    contentScale = ContentScale.Crop,
                     contentDescription = "Graphic",
+                    modifier = Modifier.fillMaxHeight()
                 )
             }
         }
