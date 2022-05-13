5c847da4b5a9d3bdc4c31a26cbb16b80866fee48
pecodeliar <92639084+pecodeliar@users.noreply.github.com>
2022-05-13 19:44:26 -0400

    Added Search Suggestion Data

--- app/src/main/java/com/example/googlemock/screen_collections/repository/ShowSuggestionRepository.kt
+++ app/src/main/java/com/example/googlemock/screen_collections/repository/ShowSuggestionRepository.kt
@@ -5,7 +5,36 @@ import com.example.googlemock.screen_collections.model.Suggestion
 class ShowSuggestionRepository {
     fun getShowSuggestionData() : List<Suggestion> {
         return listOf(
-
+            Suggestion(
+                search = "what to watch"
+            ),
+            Suggestion(
+                search = "free movies"
+            ),
+            Suggestion(
+                search = "new movies"
+            ),
+            Suggestion(
+                search = "binge worthy shows"
+            ),
+            Suggestion(
+                search = "comedy movies"
+            ),
+            Suggestion(
+                search = "documentary movies"
+            ),
+            Suggestion(
+                search = "best anime"
+            ),
+            Suggestion(
+                search = "cartoon shows"
+            ),
+            Suggestion(
+                search = "family movies"
+            ),
+            Suggestion(
+                search = "crime shows"
+            )
         )
     }
 }
