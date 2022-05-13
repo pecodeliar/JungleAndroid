017ad363be70aa5d4f763b9bf303ab1b36fbba8e
pecodeliar <92639084+pecodeliar@users.noreply.github.com>
2022-05-13 07:29:07 -0400

    Added Search Suggestion Skeletons

---
+++ app/src/main/java/com/example/googlemock/screen_collections/repository/ShowSuggestionRepository.kt
@@ -0,0 +1,11 @@
+package com.example.googlemock.screen_collections.repository
+
+import com.example.googlemock.screen_collections.model.Suggestion
+
+class ShowSuggestionRepository {
+    fun getShowSuggestionData() : List<Suggestion> {
+        return listOf(
+
+        )
+    }
+}
