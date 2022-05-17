e8451b346c58fdeea9498fc01e348a4ea38e1223
pecodeliar <92639084+pecodeliar@users.noreply.github.com>
2022-05-16 21:37:02 -0400

    Added Remaining Sections to Collections Screen

--- app/src/main/java/com/example/googlemock/screen_collections/presentation/CollectionsScreen.kt
+++ app/src/main/java/com/example/googlemock/screen_collections/presentation/CollectionsScreen.kt
@@ -4,10 +4,8 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Divider
-import androidx.compose.material.Icon
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -168,6 +166,149 @@ fun CollectionsScreen() {
                             itemContent = {
                                 SearchSuggestion(search = it)
                             }
+                        )
+                    }
+                }
+            }
+            item {
+                Column(modifier = Modifier.padding(top = 4.dp)) {
+                    Divider(color = CardButton, thickness = 1.dp)
+                    Divider(color = Primary, thickness = 6.dp)
+                }
+            }
+            //Recipes Section
+            item {
+                Column(
+                    modifier = Modifier.padding(10.dp),
+                    verticalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(
+                            text = "Recipes",
+                            color = Color.White,
+                            fontSize = 23.sp,
+                            fontWeight = FontWeight.ExtraBold,
+                            modifier = Modifier.padding(15.dp)
+                        )
+                        Icon(
+                            painter = painterResource(id = R.drawable.col_info),
+                            contentDescription = "Information Icon",
+                            tint = Accent,
+                            modifier = Modifier.size(17.dp)
+                        )
+                    }
+                    PlaceholderCard(placeholder = Placeholder.RECIPES)
+                    Text(
+                        text = "People also search for",
+                        color = Accent,
+                        fontWeight = FontWeight.ExtraBold,
+                        modifier = Modifier.padding(7.dp)
+                    )
+                    LazyRow(contentPadding = PaddingValues(top = 8.dp, bottom = 10.dp)) {
+                        items(
+                            items = recipes,
+                            itemContent = {
+                                SearchSuggestion(search = it)
+                            }
+                        )
+                    }
+                }
+            }
+            item {
+                Column(modifier = Modifier.padding(top = 4.dp)) {
+                    Divider(color = CardButton, thickness = 1.dp)
+                    Divider(color = Primary, thickness = 6.dp)
+                }
+            }
+            //Books Section
+            item {
+                Column(
+                    modifier = Modifier.padding(10.dp),
+                    verticalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(
+                            text = "Books",
+                            color = Color.White,
+                            fontSize = 23.sp,
+                            fontWeight = FontWeight.ExtraBold,
+                            modifier = Modifier.padding(15.dp)
+                        )
+                        Icon(
+                            painter = painterResource(id = R.drawable.col_info),
+                            contentDescription = "Information Icon",
+                            tint = Accent,
+                            modifier = Modifier.size(17.dp)
+                        )
+                    }
+                    PlaceholderCard(placeholder = Placeholder.BOOKS)
+                    Text(
+                        text = "People also search for",
+                        color = Accent,
+                        fontWeight = FontWeight.ExtraBold,
+                        modifier = Modifier.padding(7.dp)
+                    )
+                    LazyRow(contentPadding = PaddingValues(top = 8.dp, bottom = 10.dp)) {
+                        items(
+                            items = books,
+                            itemContent = {
+                                SearchSuggestion(search = it)
+                            }
+                        )
+                    }
+                }
+            }
+            item {
+                Column(modifier = Modifier.padding(top = 4.dp)) {
+                    Divider(color = CardButton, thickness = 1.dp)
+                    Divider(color = Primary, thickness = 6.dp)
+                }
+            }
+            //Collections Section
+            item {
+                Column(
+                    modifier = Modifier.padding(10.dp),
+                    verticalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(
+                            text = "Collections",
+                            color = Color.White,
+                            fontSize = 23.sp,
+                            fontWeight = FontWeight.ExtraBold,
+                            modifier = Modifier.padding(15.dp)
+                        )
+                        Icon(
+                            painter = painterResource(id = R.drawable.col_info),
+                            contentDescription = "Information Icon",
+                            tint = Accent,
+                            modifier = Modifier.size(17.dp)
+                        )
+                    }
+                    BookmarkBox(bookmark = Bookmark.COLLECTIONS)
+                    Button(onClick = { /*TODO*/ },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(10.dp),
+                        colors = ButtonDefaults.buttonColors(backgroundColor = Color.DarkGray),
+                        shape = RoundedCornerShape(50)
+                    ) {
+                        Text(
+                            text = "Create a collection",
+                            modifier = Modifier.fillMaxHeight(),
+                            color = Accent,
                         )
                     }
                 }
