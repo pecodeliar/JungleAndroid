f07764f8e6243f557ba7ab19ede0386219216b85
pecodeliar <progretivity@gmail.com>
2022-05-18 17:57:46 -0400

    Changed Search Bar for Discover Screen from Text Field to Button

--- app/src/main/java/com/example/googlemock/screen_discover/components/SearchBar.kt
+++ app/src/main/java/com/example/googlemock/screen_discover/components/SearchBar.kt
@@ -1,15 +1,14 @@
 package com.example.googlemock.screen_discover.components
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
@@ -18,11 +17,13 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.googlemock.R
 import com.example.googlemock.common.Screen
 import com.example.googlemock.ui.theme.Accent
+import com.example.googlemock.ui.theme.CardButton
 import com.example.googlemock.ui.theme.Secondary
 
 @Composable
@@ -31,7 +32,63 @@ fun DiscoverSearchBar(
     onTextChange: (String) -> Unit,
     navController: NavController
 ) {
-    Surface(
+    Button(
+        onClick = { /*TODO*/ },
+        modifier = Modifier.fillMaxWidth().height(55.dp),
+        shape = RoundedCornerShape(50),
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = Secondary
+        ),
+        border = BorderStroke(0.5.dp, Color.Black)
+    ) {
+        Row(
+            modifier = Modifier,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Row(
+                modifier = Modifier,
+                horizontalArrangement = Arrangement.Start
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = "Search Icon",
+                    tint = Accent,
+                    modifier = Modifier.alpha(ContentAlpha.medium).padding(end = 7.dp)
+                )
+                Text(
+                    modifier = Modifier.alpha(ContentAlpha.medium),
+                    text = "Search...",
+                    color = Accent,
+                    fontSize = 18.sp
+                )
+            }
+            Spacer(modifier = Modifier.weight(1f))
+            Row(
+                modifier = Modifier,
+                horizontalArrangement = Arrangement.End
+            ) {
+                Icon(
+                    painterResource(id = R.drawable.cmn_mic),
+                    contentDescription = "Phone Icon",
+                    tint = Color.Red,
+                    modifier = Modifier.padding(
+                        end = 12.dp
+                    )
+                )
+                Icon(
+                    painterResource(id = R.drawable.dsc_camera),
+                    contentDescription = "Email Icon",
+                    tint = com.example.googlemock.ui.theme.TextButton,
+                    modifier = Modifier.padding(
+                        start = 10.dp,
+                        end = 8.dp
+                    )
+                )
+            }
+        }
+    }
+    /*Surface(
         modifier = Modifier
             .fillMaxWidth()
             .height(55.dp)
@@ -89,7 +146,7 @@ fun DiscoverSearchBar(
                 }
             }
         )
-    }
+    }*/
 }
 
 @Preview
